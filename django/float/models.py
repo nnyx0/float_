409e5e55caea5757f6c73ea4b11c60873b71cf3c
Luke Byrnes <luke.e.byrnes@gmail.com>
2026-05-15 23:15:08 +1000

    Change sender/recipient return value order in drop-downs for messages

--- django/float/models.py
+++ django/float/models.py
@@ -48,7 +48,7 @@ class Operator(models.Model):
         help_text='Provide the order in which you wish to have this operator appear in the Message dropdown.')
 
     def __str__(self):
-        return f'{self.name} {self.base.callsign} ({self.base})' # returns the Operator's name, role, and assigned location
+        return f'{self.base} {self.base.callsign} ({self.name})' # returns the Operator's name, role, and assigned location
         # this helps reconcile the reported location of the Operator with their assigned location and the reported
         # location to assess if further support is required in the field.
 
